b9634832dfd2bf605ad18d373f5e15fd2e9a53eb
Danylo Sokol <dansokol677@gmail.com>
2021-10-15 15:41:58 +0200

    finished hamburger menu

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ let openWindow = document.querySelectorAll(".open-window");
 let popupWindow = document.querySelectorAll(".window");
 let close = document.querySelectorAll(".close");
 
-
 for(let i = 0; i < openWindow.length; i++) {
     openWindow[i].onclick = function() {
             popupWindow[i].style.display = "block";
@@ -14,5 +13,22 @@ for(let i = 0; i < openWindow.length; i++) {
         if(e.target === popupWindow[i]){
             popupWindow[i].style.display = "none";
         }
+    }
+}
+
+let menuButton = document.querySelector("a i[class='fa fa-bars']");
+
+menuButton.onclick = function(){
+    let menu = document.querySelector(".hamburger");
+    let background = document.getElementById("headerBackground");
+    if(menu.style.display === 'none' && window.innerWidth <= 899){
+        menu.style.display = 'block';
+        background.style.marginTop = '16.25rem';
+        background.style.height = 'calc(100vh - 16.25rem)';
+    }
+    else{
+        menu.style.display = 'none';
+        background.style.marginTop = '3.25rem';
+        background.style.height = 'calc(100vh - 3.25rem)';
     }
 }
